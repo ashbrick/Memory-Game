595624a6199e2e3d1511642cc19ac7bf24bc63d1
Ashley Brickhouse <ashleycbrickhouse@gmail.com>
2019-09-19 15:00:38 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,24 @@
-const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = [];
-const cardOne = cards[0];
-const cardTwo = cards[2];
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+let cardID = cards
 
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]) {
-	alert("You found a match!")
-} else {
-	alert("Sorry, try again.")
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
 }
+
+function flipCard(cardId) {
+	cardsInPlay.push(cards[cardId]);
+		console.log("User flipped" + cards[cardId]);
+
+
+	if (cardsInPlay.length === 2)
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
