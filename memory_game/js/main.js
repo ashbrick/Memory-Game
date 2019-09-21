51214e20e5145438e9b9d79e4b707049e35d7969
Ashley Brickhouse <ashleycbrickhouse@gmail.com>
2019-09-21 14:19:04 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,47 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
-let cardID = cards
+let cardId = cards
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
-}
+};
 
 function flipCard(cardId) {
-	cardsInPlay.push(cards[cardId]);
-		console.log("User flipped" + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+		console.log("User flipped " + cards[cardId].rank);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
 
-
-	if (cardsInPlay.length === 2)
+	if (cardsInPlay.length === 2) {
 	checkForMatch();
 }
+};
 
 flipCard(0);
 flipCard(2);
