171fb0ce9da82d32fb531d02d512b2f5a28e9ab7
Ashley Brickhouse <ashleycbrickhouse@gmail.com>
2019-09-22 13:29:33 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,6 +24,32 @@ var cards = [
 var cardsInPlay = [];
 let cardId = cards
 
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].cardImage);
+	console.log("User flipped " + cards[cardId].suit);
+		if (cardsInPlay.length === 2) {
+		checkForMatch(cardId);
+}
+};
+
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -32,17 +58,6 @@ function checkForMatch() {
 	}
 };
 
-function flipCard(cardId) {
-	cardsInPlay.push(cards[cardId].rank);
-		console.log("User flipped " + cards[cardId].rank);
-		console.log(cards[cardId].cardImage);
-		console.log(cards[cardId].suit);
 
-	if (cardsInPlay.length === 2) {
-	checkForMatch();
-}
-};
 
-flipCard(0);
-flipCard(2);
-
+createBoard();
